086574f085a12fa1418a6e70c709b07bee8c5ba8
harleqat <harleqat@gmail.com>
2017-01-22 16:42:03 -0500

    tested

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 def alphabetize(arr)
   # code here
+  esperanto = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by {|phrase|
+  	phrase.split('').map {|c|
+  		esperanto.index(c)
+  	}
+  }
 end
+
+alphabetize(["ĝuste", "ĝis revido"])
